a04e2a10cde9c43cb79843fbe7822aa0761fb525
Jehian <55863992+jehianth@users.noreply.github.com>
2023-09-10 14:57:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -46,27 +46,21 @@ Matkul: Pemrograman Web I Kelas A -->
                         $angka1 = $_POST['angka1'];
                         $angka2 = $_POST['angka2'];
                         $operasi = $_POST['operasi'];
+                        // init var hasil kosong kalo kondisi dibawah tidak ada yang pas
+                        $hasil = "";
                         // cek operasi apa yang dipilih
-                        switch ($operasi) {
-                            case '+':
-                                $hasil = $angka1 + $angka2;
-                                break;
-                            case '-':
-                                $hasil = $angka1 - $angka2;
-                                break;
-                            case '*':
-                                $hasil = $angka1 * $angka2;
-                                break;
-                            case '/':
-                                if ($angka2 != 0) {
-                                    $hasil = $angka1 / $angka2;
-                                } else {
-                                    $hasil = "Tidak bisa dibagi dengan nol";
-                                }
-                                break;
-                            default:
-                                $hasil = "Operasi tidak valid";
-                                break;
+                        if ($operasi == '+') {
+                            $hasil = $angka1 + $angka2;
+                        } elseif ($operasi == '-') {
+                            $hasil = $angka1 - $angka2;
+                        } elseif ($operasi == '*') {
+                            $hasil = $angka1 * $angka2;
+                        } elseif ($operasi == '/') {
+                            if ($angka2 != 0) {
+                                $hasil = $angka1 / $angka2;
+                            } else {
+                                $hasil = "Tidak bisa dibagi dengan nol";
+                            }
                         }
                         // tampilkan hasil
                         echo '<input name="hasil" type="text" value="' . $hasil . '" readonly>';
